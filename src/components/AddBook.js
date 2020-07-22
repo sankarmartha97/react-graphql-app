20c6fdbd74db86cd67a2aa859ba128c33e14d9ab
Sankar Martha <sankarmartha.123@gmail.com>
2020-07-22 23:58:26 +0530

    added refetch query

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {graphql} from 'react-apollo';
 // compose use for adding more then one queery . 
 import {flowRight as compose} from 'lodash';
-import { getAuthorsQuery, addBookMutation } from '../components/queries/queries'
+import { getAuthorsQuery, addBookMutation, getBooksQuery } from '../components/queries/queries'
 
 
 
@@ -34,7 +34,8 @@ submitFrom(e){
             name:this.state.name,
             genre:this.state.genre,
             authorId:this.state.authorId
-        }
+        },
+        refetchQueries:[{ query:getBooksQuery }]
     });
 }
 render(){
