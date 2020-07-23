a7ab40640b46e010cbcd4122c5ad1fdc4e6d4ed7
Sankar Martha <sankarmartha.123@gmail.com>
2020-07-23 17:30:15 +0530

    single query added

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import {graphql} from 'react-apollo';
-import {getBooksQuery} from '../components/queries/queries'
+import {getBooksQuery} from './queries/queries';
 
+//components
+import BookDetails from './BookDetails'
 
 // function BookList() {
 //   return (
@@ -15,6 +17,12 @@ import {getBooksQuery} from '../components/queries/queries'
 // }
 
 class BookList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selected:null
+    }
+  }
   displayBooks(){
     var data = this.props.data;
     if(data.loading){
@@ -22,7 +30,7 @@ class BookList extends Component {
     }else{
       return data.books.map(book => {
         return(
-          <li key={ book.id }>{book.name}</li>
+          <li key={ book.id } onClick={ (e)=>{ this.setState({ selected:book.id })}}>{book.name}</li>
         );
       })
     }
@@ -34,6 +42,7 @@ class BookList extends Component {
         <ul id="book-list">
             {this.displayBooks()}
         </ul>
+        <BookDetails bookId={this.state.selected}/>
       </div>
     );
   }
